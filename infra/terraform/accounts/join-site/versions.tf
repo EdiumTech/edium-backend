7fa187da1372ca5f08bd647c06646ee33781dc63
Vladislav Pankratov <pankratovvlad1@gmail.com>
2026-09-08 11:41:32 +0300

    Fix Yandex state backend initialization

--- infra/terraform/accounts/join-site/versions.tf
+++ infra/terraform/accounts/join-site/versions.tf
@@ -26,6 +26,8 @@ terraform {
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
 }
 
